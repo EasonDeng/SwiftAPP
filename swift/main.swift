748f9a34c3eb4597cd9e82b82c3aa97379a4c8cb
Eason <EasonDeng@easontekimacbook-air.local>
2015-12-10 15:44:41 +0800

    Initial Commit

---
+++ swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift
+//
+//  Created by Eason on 2015/12/10.
+//  Copyright © 2015年 Eason.com.tw. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
