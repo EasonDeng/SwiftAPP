94bf9bbf53c36f0af88dd1acb3a03046f3a846ba
Eason <EasonDeng@easontekimacbook-air.local>
2016-07-27 16:41:42 +0800

    歌詞app

---
+++ Music App/Music App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Music App
+//
+//  Created by Eason on 2016/7/20.
+//  Copyright © 2016年 Eason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
